ceafc245cdc08220ad483a2286daa05a8bcb61e8
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2024-12-01 15:53:56 +0545

    clean

--- backend_flask/src/controllers/demo_controller.py
+++
@@ -1,9 +0,0 @@
-from flask import g
-def login():
-    # here we will autehnticate user and 
-    # if true we will set cookie and return user details
-    print(g.request_json_body)
-    return "Login route"
-
-def get_details():
-    return "Get details route"
--- backend_flask/src/routes/demo_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, g
-from ..controllers.demo_controller import  get_details
-from ..middlewares.protect import protect
-
-demo_routers = Blueprint("api", __name__)
-
-
-demo_routers.add_url_rule("/all", view_func=lambda: protect(get_details)(), methods=["GET"])
